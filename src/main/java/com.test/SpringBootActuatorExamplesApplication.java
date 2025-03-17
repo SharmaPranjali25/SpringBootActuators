2533197229c75b7f09def8e91a7749852abb249f
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-17 17:17:25 +0530

    Create SpringBootActuatorExamplesApplication.java

---
+++ src/main/java/com.test/SpringBootActuatorExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorExamplesApplication.class, args);
+	}
+
+}
